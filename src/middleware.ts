008cdc039586ce89c79ca93d1ca66cd106bd4b28
LukFil <lukas.p.filipcik@gmail.com>
2024-11-03 08:44:37 +0100

    fix: add icons to matcher ignore

--- src/middleware.ts
+++ src/middleware.ts
@@ -43,6 +43,6 @@ export function middleware(request: NextRequest) {
 
 export const config = {
     matcher: [
-        "/((?!api|_next/static|_next/image|favicon.ico|manifest.json).*)",
+        "/((?!api|_next/static|_next/image|favicon.ico|manifest.json|icons).*)",
     ],
 };

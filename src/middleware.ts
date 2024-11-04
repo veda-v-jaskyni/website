f1185f2395c5273e6006e7c45dd4537cf9c9d74c
LukFil <lukas.p.filipcik@gmail.com>
2024-11-04 22:24:24 +0100

    feat: add basic CMS config

--- src/middleware.ts
+++ src/middleware.ts
@@ -45,6 +45,6 @@ export function middleware(request: NextRequest) {
 
 export const config = {
     matcher: [
-        "/((?!api|_next/static|_next/image|favicon.ico|manifest.json|icons).*)",
+        "/((?!api|_next/static|_next/image|favicon.ico|manifest.json|icons|admin).*)",
     ],
 };
